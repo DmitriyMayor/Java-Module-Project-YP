0931cb15980a197236158c83bfd45bc509820fc2
dmitriymayorov <dmitriy85mayor@gmail.com>
2025-01-15 09:00:54 +0300

    Проектная работа №1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,42 @@
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name;
+        int speed;
+
+        Race race = new Race();
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Введите название машины № " + i);
+            name = scanner.next();
+            while (true) {
+                System.out.println("Введите скорость машины № " + i);
+
+                while (true) {
+                    if (scanner.hasNextInt()) {
+                        speed = scanner.nextInt();
+                        break;
+                    } else {
+                        scanner.next();
+                    }
+                        System.out.println("Некорректный ввод! Введите целое число.");
+                }
+
+                if (speed < 250 && speed > 0) {
+                    break;
+                }
+                System.out.println("Некорректная скорость, повторите ввод!");
+            }
+
+            Car car = new Car(name, speed);
+            race.leaderCalculation(car);
+        }
+
+        System.out.println("Самая быстрая машина: " + race.leader);
+        scanner.close();
     }
 }
